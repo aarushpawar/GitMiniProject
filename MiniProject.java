5be02119345031447e0befad37f8b6e589449133
HUNTER-prog <vivekbanker07@gmail.com>
2025-11-14 11:49:04 -0500

    Part 1 Commit
    Committing Part 1 with the Square Array Method

--- MiniProject.java
+++ MiniProject.java
@@ -1,4 +1,18 @@
 public class MiniProject {
+
+	public static int[][] ArraySquare(int size)
+	{
+		int[][] array = new int[size][size];
+
+				for (int i = 0; i < size; i++) {
+					for (int j = 0; j < size; j++) {
+						array[i][j] = (int)(Math.random() * 10 + 1);
+					}
+				}
+
+		return array;
+	}
+
 	public static void main(String[] args) {
 
 	}
