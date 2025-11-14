7def2b42a2a32061313071e3cfaf5e04be9891c6
HUNTER-prog <vivekbanker07@gmail.com>
2025-11-14 11:58:50 -0500

    Part 2.1 Commit
    Commit for Part 2.1 with the first even number displayed

--- MiniProject.java
+++ MiniProject.java
@@ -4,14 +4,26 @@ public class MiniProject {
 	{
 		int[][] array = new int[size][size];
 
-				for (int i = 0; i < size; i++) {
-					for (int j = 0; j < size; j++) {
-						array[i][j] = (int)(Math.random() * 10 + 1);
-					}
-				}
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				array[i][j] = (int)(Math.random() * 10 + 1);
+			}
+		}
 
 		return array;
 	}
+
+	public int firstevennum(int arr[][])
+	{
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				if(((arr[i][j])%2)==0)
+					return arr[i][j];
+			}
+		}
+		return -1;
+	}
+
 
 	public static void main(String[] args) {
 
